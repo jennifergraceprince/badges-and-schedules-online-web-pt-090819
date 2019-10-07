024a6f13ad917373491c954c06ac73aa41e6f9d5
Jennifer Prince <jennifergraceprince@gmail.com>
2019-10-07 04:07:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,11 @@ speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_name(name)
   return "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(speakers)
+  newarray = []
+  array.each do |name|
+    newarray.push("Hello, my name is #{name}.")
+  end
+  return nuarray
+end
